393a019abb590685563149c80fafc5fa67570f4b
Vinayak Sareen <databunker@Vinayaks-MacBook-Pro.local>
2019-10-31 13:42:08 +0000

    linters added

--- index.js
+++ index.js
@@ -13,15 +13,6 @@ router.get('/', async(ctx)=>{
     ctx.body = 'Welcome to the koa-server'
 })
 
-// this is just for testing the user's actual ip and host details.
-
-
-router.get('/client_info', async(ctx)=>{
-    const ip = ctx.ip;
-    const host = ctx.host = ctx.host
-    ctx.body = {ip, host}
-})
-
 
 app.use(cors())
 app.use(register.routes())
