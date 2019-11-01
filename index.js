730641c90528c607a69709fe2f72abb9f9bff20d
Vinayak Sareen <databunker@Vinayaks-MacBook-Pro.local>
2019-11-01 17:58:21 +0000

    merge conflicts resolved

--- index.js
+++ index.js
@@ -1,46 +1,28 @@
 const Koa = require('koa')
 const Router = require('koa-router')
-<<<<<<< HEAD
 const cors = require('@koa/cors')
-=======
 const passport = require('koa-passport')
->>>>>>> feature/facebookAuth
 
-
+const register = require('./Routes/register')
 const facebookAuthRouter = require('./Routes/facebookAuth')
-const registerRoute = require('./Routes/register')
+
 const app = new Koa()
 const router = new Router()
-<<<<<<< HEAD
-const register = require('./Routes/register')
+
 
 const port = process.env.PORT || 5050
-=======
-const port = process.env.port || 222
->>>>>>> feature/facebookAuth
+
 
 
 router.get('/', async(ctx)=>{
     ctx.body = 'Welcome to the koa-server'
 })
 
-<<<<<<< HEAD
-// this is just for testing the user's actual ip and host details.
-
-
-router.get('/client_info', async(ctx)=>{
-    const ip = ctx.ip;
-    const host = ctx.host = ctx.host
-    ctx.body = {ip, host}
-})
-
 
 app.use(cors())
 app.use(register.routes())
-=======
 app.use(passport.initialize())
-app.use(registerRoute.routes())
+
 app.use(facebookAuthRouter.routes())
->>>>>>> feature/facebookAuth
 app.use(router.routes())
 app.listen(port, () => console.log(`The server is listening on port ${port}`))
