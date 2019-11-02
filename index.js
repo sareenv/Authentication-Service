803d769ea53a29ddd4daa4387a0bafff7bfbb017
Khadija <Raheemk@uni.coventry.ac.uk>
2019-11-02 13:58:34 +0000

    changes

--- index.js
+++ index.js
@@ -8,16 +8,10 @@ const passport = require('koa-passport')
 const register = require('./Routes/register')
 const facebookAuthRouter = require('./Routes/facebookAuth')
 
-<<<<<<< HEAD
 const loginRouter = require('./Routes/login')
 const deleteRouter = require('./Routes/delete')
-=======
-
-const loginRouter = require('./login')
-const deleteRouter = require('./delete')
-const updateRouter = require('./update')
+const updateRouter = require('./Routes/update')
 //const addRouter = require('./addUser')
->>>>>>> feature/update
 
 const app = new Koa() // this is ur server api
 const router = new Router()
@@ -38,6 +32,5 @@ app.use(router.routes())
 app.use(loginRouter.routes())
 app.use(deleteRouter.routes())
 app.use(updateRouter.routes())
-//app.use(addRouter.routes())
 
 app.listen(port, () => console.log(`The server is listening on port ${port}`))
