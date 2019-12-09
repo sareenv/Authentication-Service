ed79f664144bf5b37b233565b75585166e9a8ba3
vinayak <sareenv@uni.coventry.ac.uk>
2019-12-09 13:31:59 +0000

    cors changes

--- index.js
+++ index.js
@@ -26,8 +26,12 @@ const port = process.env.PORT || 5050
 router.get('/', async(ctx)=>{
     ctx.body = 'Welcome to the KVAUTH koa-server'
 })
-
-
+app.use(function(cnx, next) {
+  cnx.header("Access-Control-Allow-Origin", '*');
+  cnx.header("Access-Control-Allow-Credentials", true);
+  cnx.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  next();
+});
 
 app.use(cors())
 app.use(passport.initialize())
